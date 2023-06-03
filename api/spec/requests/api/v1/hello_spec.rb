dd70435d5323e09a429358051cc7dc0ef2822203
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-03 19:17:32 +0900

    lintを通した

--- api/spec/requests/api/v1/hello_spec.rb
+++ api/spec/requests/api/v1/hello_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Hello" do
-  describe "GET /api/v1/hello" do
+RSpec.describe 'Api::V1::Hello' do
+  describe 'GET /api/v1/hello' do
     it 'returns Hello, World!' do
       expect(response.body).to eq 'Hello, World!'
     end
