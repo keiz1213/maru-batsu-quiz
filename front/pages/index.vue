2e6eb76ace705dd032d566176d773d14e2153444
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-03 18:02:15 +0900

    vitest追加

---
+++ front/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  const name = ref<string>('world')
+</script>
+
+<template>
+  <div>
+    <input v-model="name" type="text" />
+    <Helloworld :name="name" />
+  </div>
+</template>
