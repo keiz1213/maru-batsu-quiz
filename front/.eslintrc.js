0ff0ccf9ee9675e59fd5a659f8a2448366b089ff
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-03 16:58:58 +0900

    eslintの設定を見直した

---
+++ front/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended'
+  ],
+  perser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'vue'],
+  rules: { '@typescript-eslint/no-var-requires': 0 }
+}
