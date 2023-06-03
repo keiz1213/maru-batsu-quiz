8137eedb0389db851c8c5765358940c444018041
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-03 21:49:17 +0900

    hello_spec修正

--- api/spec/requests/api/v1/hello_spec.rb
+++ api/spec/requests/api/v1/hello_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Api::V1::Hello' do
   describe 'GET /api/v1/hello' do
     it 'returns Hello, World!' do
+      get '/api/v1/hello'
       expect(response.body).to eq 'Hello, World!'
     end
   end
