0b5fc219cd98787419392974c23c9296810b4f8a
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-03 21:58:41 +0900

    useMyFetch作成

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,2 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({})
+export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      baseURL: process.env.API_URL
+    }
+  }
+})
