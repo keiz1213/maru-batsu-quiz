379f343773a7a692d788b2a4b95a14b371e6efc0
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-03 19:14:42 +0900

    hello worldを返すcontrollerを作成

---
+++ api/spec/requests/api/v1/hello_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Hello" do
+  describe "GET /api/v1/hello" do
+    it 'returns Hello, World!' do
+      expect(response.body).to eq 'Hello, World!'
+    end
+  end
+end
