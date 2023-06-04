2d2898046fcac9ebb6e725425efe744182ec3125
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-04 23:49:31 +0900

    レイアウト追加

---
+++ front/layouts/MbqAfterLogin.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  const { signOut, currentUser } = useAuth()
+
+  const logout = async (): Promise<void> => {
+    await signOut()
+    navigateTo('/login', { replace: true })
+    console.log('logoutしました')
+  }
+  const withdrawal = async (): Promise<void> => {
+    await useFetch(
+      `http://localhost:3001/api/v1/users/${currentUser.value.id}`,
+      { method: 'delete' }
+    )
+    await signOut()
+    navigateTo('/withdrawal', { replace: true })
+    console.log('退会しました')
+  }
+</script>
+
+<template>
+  <MbqNavBar @logout="logout"></MbqNavBar>
+  <slot />
+  <MbqFooter @withdrawal="withdrawal"></MbqFooter>
+</template>
---
+++ front/layouts/beforeLogin.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="font-semibold text-xl tracking-tight">Before Login</h1>
+    <slot />
+  </div>
+</template>
