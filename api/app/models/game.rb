d2d23b17669632ba60a1311296cf50b2fcb2216e
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-04 16:56:42 +0900

    リソース間のリレーション設定

--- api/app/models/game.rb
+++ api/app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+  belongs_to :user
+  has_many :quizzes, dependent: :destroy
+  accepts_nested_attributes_for :quizzes
 end
--- api/app/models/quiz.rb
+++ api/app/models/quiz.rb
@@ -1,2 +1,3 @@
 class Quiz < ApplicationRecord
+  belongs_to :game
 end
