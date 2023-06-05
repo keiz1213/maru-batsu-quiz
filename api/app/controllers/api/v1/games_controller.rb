c5cc1695561ad38a46ac09cde07dafa6d877b768
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-05 13:08:23 +0900

    gamesコントローラ作成

---
+++ api/app/controllers/api/v1/games_controller.rb
@@ -0,0 +1,60 @@
+class Api::V1::GamesController < ApplicationController
+  def show
+    game = Game.find(params[:id])
+    render json: game.as_json(include: :quizzes)
+  end
+
+  def create
+    game = Game.new(game_params)
+    buildQuizzes(game)
+    game.save
+    render json: game.as_json(include: :quizzes)
+  end
+
+  def update
+    game = Game.find(params[:id])
+    rebuildQuizzes(game)
+    game.update(game_params)
+  end
+
+  def destroy
+    game = Game.find(params[:id])
+    game.destroy
+  end
+
+
+  private
+
+  def game_params
+    params.require(:game).permit(
+      :user_id,
+      :title,
+      :description,
+      :number_of_winner,
+      :channel_name
+    )
+  end
+
+  def buildQuizzes(game)
+    params.require(:quizzes).each do |quiz|
+      permitted_params = quiz.permit(
+        :question,
+        :correct_answer,
+        :explanation
+      )
+      game.quizzes.build(permitted_params)
+    end
+  end
+
+  def rebuildQuizzes(game)
+    game.quizzes.destroy_all
+    params.require(:quizzes).each do |quiz|
+      permitted_params = quiz.permit(
+        :question,
+        :correct_answer,
+        :explanation
+      )
+      game.quizzes.build(permitted_params)
+    end
+  end
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :hello, only:[:index]
       resources :users, only:[:create]
+      resources :games, only:[:create, :show, :destroy, :update]
       resources :skyway_token, only:[:create]
     end
   end
