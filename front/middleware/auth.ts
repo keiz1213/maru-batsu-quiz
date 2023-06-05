f3a799cc8baccffe53b0d066bbfab304a814d7a5
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-05 17:01:54 +0900

    本番環境で動作確認するために一時的にmiddlewareを無効化

--- front/middleware/auth.ts
+++ front/middleware/auth.ts
@@ -1,8 +1,9 @@
 export default defineNuxtRouteMiddleware(async () => {
-  const { checkAuthState, currentUser } = useAuth()
-  await checkAuthState()
-  if (currentUser.value.uid === '') {
-    console.log('ログイン状態を確認できません by middleware: checkAuth')
-    return await navigateTo('/login')
-  }
+  // const { checkAuthState, currentUser } = useAuth()
+  // await checkAuthState()
+  // if (currentUser.value.uid === '') {
+  //   console.log('ログイン状態を確認できません by middleware: checkAuth')
+  //   return await navigateTo('/login')
+  // }
+  console.log('middleware')
 })
