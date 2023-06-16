eb9fb04603841106e4206598ece3fcdd73b7491e
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-17 03:33:43 +0900

    問題文が長い時にスクロールできるようにした

--- front/components/venue/QuestionBoard.vue
+++ front/components/venue/QuestionBoard.vue
@@ -8,13 +8,13 @@
 
 <template>
   <div
-    class="w-[950px] h-[270px] p-4 bg-white border border-gray-200 rounded-lg break-all"
+    class="w-[950px] h-[270px] p-4 bg-white border border-gray-200 rounded-lg break-all overflow-y-scroll"
   >
     <div class="flex">
       <div id="announce-container" class="w-[885px]">
-        <p class="text-3xl break-words whitespace-pre-wrap">
+        <div class="text-3xl break-words whitespace-pre-wrap">
           {{ announcement }}
-        </p>
+        </div>
       </div>
       <div id="timer-container">
         <Timelimit :elapsed="elapsed" :limit="limit" />
