170c3462091228a53cefaa02f551de8e76e1b2ce
keiz1213 <fukuoka00tsc@gmail.com>
2023-07-07 15:20:32 +0900

    利用規約追加

--- front/pages/privacy.vue
+++ front/pages/privacy.vue
@@ -17,50 +17,50 @@
         </h2>
       </div>
       <div>
-        <h3 class="text-xl font-bold mb-3">第1条(収集する情報)</h3>
+        <h3 class="text-xl font-bold mb-3">第1条 (収集する情報)</h3>
         <p class="mb-3">本サービスでは、以下の情報を収集することがあります。</p>
-        <ul class="list-disc list-inside indent-4">
+        <ul class="list-decimal ml-10">
           <li>GitHubアカウント情報（ユーザー名、メールアドレスなど）</li>
           <li>クイズの回答履歴や成績などのゲームプレイ情報</li>
         </ul>
       </div>
       <div>
-        <h3 class="text-xl font-bold mb-3">第2条(収集する目的)</h3>
+        <h3 class="text-xl font-bold mb-3">第2条 (収集する目的)</h3>
         <p class="mb-3">収集した情報は、以下の目的で利用されます。</p>
-        <ul class="list-disc list-inside indent-4">
+        <ul class="list-decimal ml-10">
           <li>ユーザーの認証およびアカウント管理</li>
           <li>サービスの改善および新機能の開発</li>
         </ul>
       </div>
       <div>
         <h3 class="text-xl font-bold mb-3">
-          第3条(情報の共有と第三者への提供)
+          第3条 (情報の共有と第三者への提供)
         </h3>
         <p class="mb-3">
           本サービスでは、ユーザーの個人情報を適切に保護し、第三者に提供することはありません。ただし、法的な要求や規制に基づく場合、またはユーザーの同意がある場合に限り、情報を提供することがあります。
         </p>
       </div>
       <div>
-        <h3 class="text-xl font-bold mb-3">第4条(セキュリティ対策)</h3>
+        <h3 class="text-xl font-bold mb-3">第4条 (セキュリティ対策)</h3>
         <p class="mb-3">
           本サービスでは、ユーザーの個人情報の機密性を保護するために適切なセキュリティ対策を実施しています。情報の不正アクセス、紛失、漏洩、改ざんなどに対するリスクを最小限に抑えるために努力しています。
         </p>
       </div>
       <div>
-        <h3 class="text-xl font-bold mb-3">第5条(Cookieの使用)</h3>
+        <h3 class="text-xl font-bold mb-3">第5条 (Cookieの使用)</h3>
         <p class="mb-3">
           本サービスでは、一部の機能や利便性の向上のためにCookieを使用する場合があります。これにより、ユーザーのプリファレンスやセッション情報が保存されます。Cookieに関する詳細やオプトアウトの方法については、ブラウザの設定をご確認ください。
         </p>
       </div>
       <div>
-        <h3 class="text-xl font-bold mb-3">第6条(外部リンク)</h3>
+        <h3 class="text-xl font-bold mb-3">第6条 (外部リンク)</h3>
         <p class="mb-3">
           本サービスは、他のウェブサイトへのリンクを含む場合があります。外部サイトへのアクセスや利用に関しては、本プライバシーポリシーは適用されず、それぞれのサイトのプライバシーポリシーをご確認ください。
         </p>
       </div>
       <div>
         <h3 class="text-xl font-bold mb-3">
-          第7条(プライバシーポリシーの変更)
+          第7条 (プライバシーポリシーの変更)
         </h3>
         <p class="mb-3">
           本サービスでは、プライバシーポリシーを必要に応じて変更する場合があります。重要な変更がある場合には、ユーザーに通知する方法を適切に採用します。
@@ -69,3 +69,10 @@
     </div>
   </TheContainer>
 </template>
+
+<style>
+  li {
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+</style>
