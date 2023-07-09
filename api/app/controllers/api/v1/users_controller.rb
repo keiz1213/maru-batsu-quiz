386f4a0c64fdea136bec2c72a9b57e8df7e448f5
keiz1213 <fukuoka00tsc@gmail.com>
2023-07-09 20:25:31 +0900

    users_controller.rbをリファクタリングした

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,41 @@
 class Api::V1::UsersController < ApplicationController
-  include ActionController::HttpAuthentication::Token::ControllerMethods
-
   def create
     FirebaseIdToken::Certificates.request unless FirebaseIdToken::Certificates.present?
-    token = request.headers['Authorization']&.split&.last
-    payload = FirebaseIdToken::Signature.verify(token)
-    uid = payload['sub']
-    name = payload['name']
-    avatar_url = payload['picture']
-    user = User.find_or_initialize_by(uid:) do |u|
-      u.name = name
-      u.avatar_url = avatar_url
+    raise ArgumentError, 'BadRequest Parameter' if payload.blank?
+
+    user = User.find_or_initialize_by(uid: uid_from_payload) do |u|
+      u.name = name_from_payload
+      u.avatar_url = avatar_url_from_payload
     end
-    user.games
+
     if user.save
-      user.update(name:) if user.name != name
-      user.update(avatar_url:) if user.avatar_url != avatar_url
+      user.update(name:) if user.name != name_from_payload
+      user.update(avatar_url:) if user.avatar_url != avatar_url_from_payload
       render json: user.as_json(include: :games)
     else
-      render json: { status_code: 422 }
+      render json: { status: :unprocessable_entity }
     end
+  end
+
+  private
+
+  def token_from_request_headers
+    request.headers['Authorization']&.split&.last
+  end
+
+  def payload
+    @payload ||= FirebaseIdToken::Signature.verify(token_from_request_headers)
+  end
+
+  def uid_from_payload
+    payload['sub']
+  end
+  
+  def name_from_payload
+    payload['name']
+  end
+  
+  def avatar_url_from_payload
+    payload['picture']
   end
 end
