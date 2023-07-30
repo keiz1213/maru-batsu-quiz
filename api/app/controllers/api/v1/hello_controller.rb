71bce9e02f9e0cad48b4d095774543560c4c27e5
keiz1213 <fukuoka00tsc@gmail.com>
2023-07-31 02:44:08 +0900

    skyway_tokensのrequest_spec追加

--- api/app/controllers/api/v1/hello_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::HelloController < ApplicationController
-  skip_before_action :authenticate_with_firebase_id_token!, only: %i[index]
-  def index
-    render json: 'Hello, World!'
-  end
-end
--- api/spec/requests/api/v1/skyway_token_spec.rb
+++ api/spec/requests/api/v1/skyway_token_spec.rb
@@ -1,7 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::SkywayTokens' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /api/v1/skyway_token' do
+    let(:user) { create(:user) }
+    let(:valid_headers) {{
+      Authorization: create_valid_token(user)
+    }}
+    let(:invalid_headers) {{
+      Authorization: create_invalid_token(user)
+    }}
+
+    context 'as an authenticated user' do
+      it 'returns status 200 (ok)' do
+        get api_v1_skyway_token_path, headers: valid_headers
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'as an unauthenticated user' do
+      it 'returns status 401 (unauthorized)' do
+        get api_v1_skyway_token_path, headers: invalid_headers
+        expect(response).to have_http_status(401)
+      end
+    end
   end
 end
