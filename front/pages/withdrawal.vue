ff48213acddeefa67b31f8c37f54e06b8f239ec6
keiz1213 <fukuoka00tsc@gmail.com>
2023-08-15 00:33:41 +0900

    退会完了ページにデザインをあてた

--- front/pages/withdrawal.vue
+++ front/pages/withdrawal.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+  definePageMeta({
+    layout: 'mbq-after-login'
+  })
   useHead({
     title: '退会完了'
   })
@@ -6,7 +9,11 @@
 
 <template>
   <TheContainer>
-    <p>ご利用ありがとうございました</p>
-    <NuxtLink :to="'/'">○✗クイズオンライン</NuxtLink>
+    <h2 class="text-3xl text-center mt-48">
+      ご利用ありがとうございました。またのご利用お待ちしています。
+    </h2>
+    <span class="text-2xl block text-center mt-20 underline"
+      ><NuxtLink :to="'/'">Top</NuxtLink></span
+    >
   </TheContainer>
 </template>
