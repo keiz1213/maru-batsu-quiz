0ca8f34e61851fc87d98f0f5e6675b6568fb398a
keiz1213 <fukuoka00tsc@gmail.com>
2023-08-21 02:54:09 +0900

    メインビジュアル修正

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -23,32 +23,32 @@
             <MbqButtonGithub :onClick="login" :isLoading="loading" />
           </div>
         </div>
-        <div id="right" class="max-w-full my-10">
+        <div id="right" class="max-w-full my-16">
           <div>
-            <div class="px-12 xl:px-24">
+            <div>
               <div
-                class="sm:max-w-[640px] px-[20px] bg-black mx-auto mt-10 pt-5 rounded-t-lg relative"
+                class="w-[270px] sm:w-[450px] md:w-[650px] px-[20px] bg-black mx-auto pt-4 rounded-t-lg relative"
               >
                 <div
                   class="w-[8px] h-[8px] rounded-full bg-green-500 absolute top-1 left-[47%]"
                 ></div>
                 <img
                   src="../assets/images/demo1.png"
-                  class="sm:max-w-[600px] block mx-auto"
+                  class="w-[250px] sm:w-[430px] md:w-[630px] block mx-auto"
                   alt="demo"
                 />
               </div>
             </div>
             <div class="">
               <div
-                class="sm:max-w-[800px] h-[25px] bg-slate-300 relative mx-auto"
+                class="w-[350px] h-[15px] sm:w-[550px] sm:h-[20px] md:w-[800px] md:h-[25px] bg-slate-300 relative mx-auto"
               >
                 <div
-                  class="w-[120px] h-[8px] bg-slate-400 absolute rounded-b-lg left-[35%] md:left-[40%]"
+                  class="w-[100px] h-[6px] sm:w-[120px] sm:h-[8px] bg-slate-400 absolute rounded-b-lg left-[35%] md:left-[40%]"
                 ></div>
               </div>
               <div
-                class="sm:max-w-[800px] h-[8px] bg-slate-400 rounded-b-lg mx-auto"
+                class="w-[350px] h-[6px] sm:w-[550px] md:w-[800px] bg-slate-400 rounded-b-lg mx-auto"
               ></div>
             </div>
           </div>
