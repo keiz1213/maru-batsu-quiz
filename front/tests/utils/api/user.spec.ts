9440324ee92f38d55d50f026264b9f78fa299f77
keiz1213 <fukuoka00tsc@gmail.com>
2023-09-01 01:27:37 +0900

    utils/api/servicesのテスト追加

---
+++ front/tests/utils/api/user.spec.ts
@@ -0,0 +1,75 @@
+import { postUser, deleteUser } from '~/utils/api/services/user'
+import { describe, it, expect, vi } from 'vitest'
+
+describe('user', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  const mocks = vi.hoisted(() => {
+    return {
+      useCustomFetch: vi.fn()
+    }
+  })
+
+  vi.mock('~/composables/use-custom-fetch', () => {
+    return {
+      useCustomFetch: mocks.useCustomFetch
+    }
+  })
+
+  const user = {
+    id: 1,
+    uid: 'testUid',
+    name: 'test',
+    avatar_url: 'https://example.com/photo.jpg',
+    games: []
+  }
+
+  describe('postUser', () => {
+    it('post a user', async () => {
+      mocks.useCustomFetch.mockReturnValueOnce({
+        data: { value: user },
+        error: { value: false }
+      })
+
+      const result = await postUser()
+      expect(result).toEqual(user)
+      expect(mocks.useCustomFetch).toHaveBeenCalledWith(`/api/v1/users`, {
+        method: 'post'
+      })
+    })
+
+    it('throw error', async () => {
+      mocks.useCustomFetch.mockReturnValueOnce({ error: { value: true } })
+      await expect(postUser()).rejects.toThrow(Error)
+      expect(mocks.useCustomFetch).toHaveBeenCalledWith(`/api/v1/users`, {
+        method: 'post'
+      })
+    })
+  })
+
+  describe('deleteUser', () => {
+    it('delete a user', async () => {
+      mocks.useCustomFetch.mockReturnValueOnce({
+        error: { value: false }
+      })
+      const userId = 1
+      await deleteUser(userId)
+      expect(mocks.useCustomFetch).toHaveBeenCalledWith(
+        `/api/v1/users/${user.id}`,
+        { method: 'delete' }
+      )
+    })
+
+    it('throw error', async () => {
+      mocks.useCustomFetch.mockReturnValueOnce({ error: { value: true } })
+      const userId = 1
+      await expect(deleteUser(userId)).rejects.toThrow(Error)
+      expect(mocks.useCustomFetch).toHaveBeenCalledWith(
+        `/api/v1/users/${user.id}`,
+        { method: 'delete' }
+      )
+    })
+  })
+})
