3c61345dc8f19c4c4bc273368bb1c14c2ac0f2f1
keiz1213 <fukuoka00tsc@gmail.com>
2023-09-03 10:07:38 +0900

    use-firebase-auth#firebaseWithdrawalのテスト追加

--- front/tests/composables/use-firebase-auth.spec.ts
+++ front/tests/composables/use-firebase-auth.spec.ts
@@ -6,6 +6,13 @@ afterEach(() => {
 })
 
 const mocks = vi.hoisted(() => {
+  class MockedGithubAuthProvider {
+    constructor() {}
+  
+    static credentialFromResult() {
+      return 'mockedCredential'
+    }
+  }
   return {
     getAuth: vi.fn(() => {
       return {
@@ -13,7 +20,7 @@ const mocks = vi.hoisted(() => {
       }
     }),
     signInWithPopup: vi.fn(),
-    GithubAuthProvider: vi.fn(),
+    GithubAuthProvider: MockedGithubAuthProvider,
     signOut: vi.fn(),
     onAuthStateChanged: vi.fn(),
     reauthenticateWithCredential: vi.fn(),
@@ -34,30 +41,40 @@ vi.mock('firebase/auth', async () => {
     deleteUser: mocks.deleteUser
   }
 })
-
-it('checkAuthState', () => {
-  const { checkAuthState } = useFirebaseAuth()
-  checkAuthState()
-  expect(mocks.getAuth).toHaveBeenCalledOnce()
-  expect(mocks.onAuthStateChanged).toHaveBeenCalledOnce()
+describe('checkAuthState', () => {
+  it('can keep authentication if logged in firebase', () => {
+    const { checkAuthState } = useFirebaseAuth()
+    checkAuthState()
+    expect(mocks.getAuth).toHaveBeenCalledOnce()
+    expect(mocks.onAuthStateChanged).toHaveBeenCalledOnce()
+  })
 })
 
-it('firebaseLogin', () => {
-  const { firebaseLogin } = useFirebaseAuth()
-  firebaseLogin()
-  expect(mocks.getAuth).toHaveBeenCalledOnce()
-  expect(mocks.GithubAuthProvider).toHaveBeenCalledOnce()
-  expect(mocks.signInWithPopup).toHaveBeenCalledOnce()
+describe('firebaseLogin', () => {
+  it('can login to firebase', async () => {
+    const { firebaseLogin } = useFirebaseAuth()
+    await firebaseLogin()
+    expect(mocks.getAuth).toHaveBeenCalledOnce()
+    expect(mocks.signInWithPopup).toHaveBeenCalledOnce()
+  })
 })
 
-it('firebaseLogout', () => {
-  const { firebaseLogout } = useFirebaseAuth()
-  firebaseLogout()
-  expect(mocks.getAuth).toHaveBeenCalledOnce()
-  expect(mocks.signOut).toHaveBeenCalledOnce()
+describe('firebaseLogout', () => {
+  it('can logout from firebase', async () => {
+    const { firebaseLogout } = useFirebaseAuth()
+    await firebaseLogout()
+    expect(mocks.getAuth).toHaveBeenCalledOnce()
+    expect(mocks.signOut).toHaveBeenCalledOnce()
+  })
 })
-// TO DO
-it('firebaseWithdrawal', () => {
-  // const { firebaseWithdrawal } = useFirebaseAuth()
-  // firebaseWithdrawal()
+
+describe('firebaseWithdrawal', () => {
+  it('can withdrawal from firebase', async () => {
+    const { firebaseWithdrawal } = useFirebaseAuth()
+    await firebaseWithdrawal()
+    expect(mocks.getAuth).toHaveBeenCalledOnce()
+    expect(mocks.signInWithPopup).toHaveBeenCalledOnce()
+    expect(mocks.reauthenticateWithCredential).toHaveBeenCalledOnce()
+    expect(mocks.deleteUser).toHaveBeenCalledOnce()
+  })
 })
