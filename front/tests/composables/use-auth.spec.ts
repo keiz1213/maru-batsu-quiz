252cec8ed8b997bb2e1745ab0e76089e171b9d19
keiz1213 <fukuoka00tsc@gmail.com>
2023-09-03 11:28:59 +0900

    use-authのテストをリファクタリング

--- front/tests/composables/use-auth.spec.ts
+++ front/tests/composables/use-auth.spec.ts
@@ -1,103 +1,92 @@
 // @vitest-environment nuxt
 import { vi, expect, it } from 'vitest'
-import { postUser, deleteUser } from '@/utils/api/services/user'
 
 afterEach(() => {
   vi.restoreAllMocks()
 })
 
-beforeEach(() => {
-  mocks.useLoading.mockReturnValue({
-    setLoading: vi.fn(),
-    clearLoading: vi.fn()
-  })
-
-  mocks.useToast.mockReturnValue({
-    setToast: vi.fn(),
-    notifyOnSpot: vi.fn()
-  })
-
-  mocks.useCurrentUser.mockReturnValue({
-    currentUser: vi.fn(),
-    clearCurrentUserStore: vi.fn(),
-    checkCurrentUserStore: vi.fn()
-  })
-
-  mocks.useGame.mockReturnValue({
-    clearGamesStore: vi.fn(),
-    checkGamesStore: vi.fn()
-  })
-})
-
 const mocks = vi.hoisted(() => {
   return {
-    useLoading: vi.fn(() => {
-      return {
-        setLoading: vi.fn(),
-        clearLoading: vi.fn()
-      }
-    }),
-    useToast: vi.fn(() => {
-      return {
-        setToast: vi.fn(),
-        notifyOnSpot: vi.fn()
-      }
-    }),
-    useCurrentUser: vi.fn(() => {
-      return {
-        currentUser: vi.fn(),
-        clearCurrentUserStore: vi.fn(),
-        checkCurrentUserStore: vi.fn()
-      }
-    }),
-    useGame: vi.fn(() => {
-      return {
-        clearGamesStore: vi.fn(),
-        checkGamesStore: vi.fn()
-      }
-    }),
-    useFirebaseAuth: vi.fn(() => {
-      return {
-        user: vi.fn(),
-        isLoggedIn: vi.fn(),
-        firebaseLogin: vi.fn(),
-        firebaseLogout: vi.fn(),
-        checkAuthState: vi.fn(),
-        firebaseWithdrawal: vi.fn()
-      }
-    }),
+    setLoading: vi.fn(),
+    clearLoading: vi.fn(),
+    setToast: vi.fn(),
+    notifyOnSpot: vi.fn(),
+    currentUser: vi.fn(),
+    clearCurrentUserStore: vi.fn(),
+    checkCurrentUserStore: vi.fn(),
+    clearGamesStore: vi.fn(),
+    checkGamesStore: vi.fn(),
+    value: vi.fn(),
+    isLoggedIn: vi.fn(),
+    firebaseLogin: vi.fn(),
+    firebaseLogout: vi.fn(),
+    checkAuthState: vi.fn(),
+    firebaseWithdrawal: vi.fn(),
     postUser: vi.fn(),
-    deleteUser: vi.fn()
+    deleteUser: vi.fn(),
+    navigateTo: vi.fn()
   }
 })
 
 vi.mock('~/composables/use-loading', () => {
   return {
-    useLoading: mocks.useLoading
+    useLoading: () => {
+      return {
+        setLoading: mocks.setLoading,
+        clearLoading: mocks.clearLoading
+      }
+    }
   }
 })
 
 vi.mock('~/composables/use-toast', () => {
   return {
-    useToast: mocks.useToast
+    useToast: () => {
+      return {
+        setToast: mocks.setToast,
+        notifyOnSpot: mocks.notifyOnSpot
+      }
+    }
   }
 })
 
 vi.mock('~/composables/use-game', () => {
   return {
-    useGame: mocks.useGame
+    useGame: () => {
+      return {
+        clearGamesStore: mocks.clearGamesStore,
+        checkGamesStore: mocks.checkGamesStore
+      }
+    }
   }
 })
 
 vi.mock('~/composables/use-firebase-auth', () => {
   return {
-    useFirebaseAuth: mocks.useFirebaseAuth
+    useFirebaseAuth: () => {
+      return {
+        user: {
+          value: mocks.value
+        },
+        isLoggedIn: mocks.isLoggedIn,
+        firebaseLogin: mocks.firebaseLogin,
+        firebaseLogout: mocks.firebaseLogout,
+        checkAuthState: mocks.checkAuthState,
+        firebaseWithdrawal: mocks.firebaseWithdrawal
+      }
+    }
   }
 })
 
 vi.mock('~/composables/use-current-user', () => {
   return {
-    useCurrentUser: mocks.useCurrentUser
+    useCurrentUser: () => {
+      return {
+        currentUser: mocks.currentUser,
+        clearCurrentUserStore: mocks.clearCurrentUserStore,
+        checkCurrentUserStore: mocks.checkCurrentUserStore
+      }
+    }
   }
 })
 
@@ -112,211 +101,143 @@ vi.mock('~/utils/api/services/user', async () => {
   }
 })
 
+vi.mock('nuxt/app', async () => {
+  const nuxt = (await vi.importActual('nuxt/app')) as Object
+  return {
+    ...nuxt,
+    navigateTo: mocks.navigateTo
+  }
+})
+
 describe('login', () => {
   it('can login', async () => {
-    mocks.useFirebaseAuth.mockReturnValue({
-      user: vi.fn(),
-      isLoggedIn: vi.fn(),
-      firebaseLogin: vi.fn(),
-      firebaseLogout: vi.fn(),
-      checkAuthState: vi.fn(),
-      firebaseWithdrawal: vi.fn()
-    })
-
-    const { setLoading, clearLoading } = useLoading()
-    const { setToast } = useToast()
-    const { firebaseLogin } = useFirebaseAuth()
     const { login } = useAuth()
     await login()
 
-    expect(setLoading).toHaveBeenCalledOnce()
-    expect(firebaseLogin).toHaveBeenCalledOnce()
-    expect(postUser).toHaveBeenCalledOnce()
-    expect(clearLoading).toHaveBeenCalledOnce()
-    expect(setToast).toHaveBeenCalledWith('ログインしました！', 'success')
+    expect(mocks.setLoading).toHaveBeenCalledOnce()
+    expect(mocks.firebaseLogin).toHaveBeenCalledOnce()
+    expect(mocks.postUser).toHaveBeenCalledOnce()
+    expect(mocks.clearLoading).toHaveBeenCalledOnce()
+    expect(mocks.setToast).toHaveBeenCalledWith('ログインしました！', 'success')
   })
 
   it('if Firebase Login fails, user should not be able to login', async () => {
-    mocks.useFirebaseAuth.mockReturnValue({
-      user: vi.fn(),
-      isLoggedIn: vi.fn(),
-      firebaseLogin: vi.fn().mockRejectedValue(new Error()),
-      firebaseLogout: vi.fn(),
-      checkAuthState: vi.fn(),
-      firebaseWithdrawal: vi.fn()
-    })
-    const { setLoading, clearLoading } = useLoading()
-    const { notifyOnSpot } = useToast()
-    const { firebaseLogin } = useFirebaseAuth()
+    mocks.firebaseLogin.mockRejectedValueOnce(new Error())
     const { login } = useAuth()
     await login()
 
-    expect(setLoading).toHaveBeenCalledOnce()
-    expect(firebaseLogin).toHaveBeenCalledOnce()
-    expect(clearLoading).toHaveBeenCalledOnce()
-    expect(notifyOnSpot).toHaveBeenCalledWith('ログインに失敗しました', 'error')
+    expect(mocks.setLoading).toHaveBeenCalledOnce()
+    expect(mocks.firebaseLogin).toHaveBeenCalledOnce()
+    expect(mocks.clearLoading).toHaveBeenCalledOnce()
+    expect(mocks.notifyOnSpot).toHaveBeenCalledWith(
+      'ログインに失敗しました',
+      'error'
+    )
   })
 
   it('if Post User fails, user should not be able to login', async () => {
-    mocks.useFirebaseAuth.mockReturnValue({
-      user: vi.fn(),
-      isLoggedIn: vi.fn(),
-      firebaseLogin: vi.fn(),
-      firebaseLogout: vi.fn(),
-      checkAuthState: vi.fn(),
-      firebaseWithdrawal: vi.fn()
-    })
-
-    mocks.postUser.mockRejectedValue(new Error())
-
-    const { setLoading, clearLoading } = useLoading()
-    const { notifyOnSpot } = useToast()
-    const { firebaseLogin } = useFirebaseAuth()
+    mocks.postUser.mockRejectedValueOnce(new Error())
     const { login } = useAuth()
     await login()
 
-    expect(setLoading).toHaveBeenCalledOnce()
-    expect(firebaseLogin).toHaveBeenCalledOnce()
-    expect(clearLoading).toHaveBeenCalledOnce()
-    expect(notifyOnSpot).toHaveBeenCalledWith('ログインに失敗しました', 'error')
+    expect(mocks.setLoading).toHaveBeenCalledOnce()
+    expect(mocks.firebaseLogin).toHaveBeenCalledOnce()
+    expect(mocks.clearLoading).toHaveBeenCalledOnce()
+    expect(mocks.notifyOnSpot).toHaveBeenCalledWith(
+      'ログインに失敗しました',
+      'error'
+    )
+  })
+
+  it('if the user attempts to visit a specific page, they will be redirected there after logging in.', async () => {
+    const { login } = useAuth()
+    const { setRedirectPath } = useFriendlyForwarding()
+    setRedirectPath('/games/1')
+    await login()
+
+    expect(mocks.setLoading).toHaveBeenCalledOnce()
+    expect(mocks.firebaseLogin).toHaveBeenCalledOnce()
+    expect(mocks.postUser).toHaveBeenCalledOnce()
+    expect(mocks.clearLoading).toHaveBeenCalledOnce()
+    expect(mocks.setToast).toHaveBeenCalledWith('ログインしました！', 'success')
+    expect(mocks.navigateTo).toHaveBeenCalledWith('/games/1')
   })
 })
 
 describe('logout', () => {
   it('can logout', async () => {
-    mocks.useFirebaseAuth.mockReturnValue({
-      user: vi.fn(),
-      isLoggedIn: vi.fn(),
-      firebaseLogin: vi.fn(),
-      firebaseLogout: vi.fn(),
-      checkAuthState: vi.fn(),
-      firebaseWithdrawal: vi.fn()
-    })
-
-    const { setLoading, clearLoading } = useLoading()
-    const { setToast } = useToast()
-    const { clearCurrentUserStore } = useCurrentUser()
-    const { clearGamesStore } = useGame()
-    const { firebaseLogout } = useFirebaseAuth()
     const { logout } = useAuth()
     await logout()
 
-    expect(setLoading).toHaveBeenCalledOnce()
-    expect(firebaseLogout).toHaveBeenCalledOnce()
-    expect(clearCurrentUserStore).toHaveBeenCalledOnce()
-    expect(clearGamesStore).toHaveBeenCalledOnce()
-    expect(clearLoading).toHaveBeenCalledOnce()
-    expect(setToast).toHaveBeenCalledWith('ログアウトしました！', 'success')
+    expect(mocks.setLoading).toHaveBeenCalledOnce()
+    expect(mocks.firebaseLogout).toHaveBeenCalledOnce()
+    expect(mocks.clearCurrentUserStore).toHaveBeenCalledOnce()
+    expect(mocks.clearGamesStore).toHaveBeenCalledOnce()
+    expect(mocks.clearLoading).toHaveBeenCalledOnce()
+    expect(mocks.navigateTo).toHaveBeenCalledWith('/')
+    expect(mocks.setToast).toHaveBeenCalledWith('ログアウトしました！', 'success')
   })
 
   it('if Firebase Logout fails, user should not be able to logout', async () => {
-    mocks.useFirebaseAuth.mockReturnValue({
-      user: vi.fn(),
-      isLoggedIn: vi.fn(),
-      firebaseLogin: vi.fn(),
-      firebaseLogout: vi.fn().mockRejectedValue(new Error()),
-      checkAuthState: vi.fn(),
-      firebaseWithdrawal: vi.fn()
-    })
-
-    const { clearLoading } = useLoading()
-    const { notifyOnSpot } = useToast()
-    const { firebaseLogout } = useFirebaseAuth()
-    const { clearCurrentUserStore } = useCurrentUser()
-    const { clearGamesStore } = useGame()
+    mocks.firebaseLogout.mockRejectedValueOnce(new Error())
     const { logout } = useAuth()
     await logout()
 
-    expect(clearLoading).toHaveBeenCalledOnce()
-    expect(firebaseLogout).toHaveBeenCalledOnce()
-    expect(clearCurrentUserStore).not.toHaveBeenCalledOnce()
-    expect(clearGamesStore).not.toHaveBeenCalledOnce()
-    expect(clearLoading).toHaveBeenCalledOnce()
-    expect(notifyOnSpot).toHaveBeenCalledWith(
-      'ログアウトに失敗しました',
-      'error'
-    )
+    expect(mocks.setLoading).toHaveBeenCalledOnce()
+    expect(mocks.firebaseLogout).toHaveBeenCalledOnce()
+    expect(mocks.clearCurrentUserStore).not.toHaveBeenCalledOnce()
+    expect(mocks.clearGamesStore).not.toHaveBeenCalledOnce()
+    expect(mocks.clearLoading).toHaveBeenCalledOnce()
+    expect(mocks.navigateTo).not.toHaveBeenCalledOnce()
+    expect(mocks.notifyOnSpot).toHaveBeenCalledWith('ログアウトに失敗しました', 'error')
   })
 })
 
 describe('withdrawal', () => {
   it('can withdrawal', async () => {
-    mocks.useFirebaseAuth.mockReturnValue({
-      user: vi.fn(),
-      isLoggedIn: vi.fn(),
-      firebaseLogin: vi.fn(),
-      firebaseLogout: vi.fn(),
-      checkAuthState: vi.fn(),
-      firebaseWithdrawal: vi.fn()
-    })
-
-    const { setLoading, clearLoading } = useLoading()
-    const { firebaseWithdrawal } = useFirebaseAuth()
-    const { clearCurrentUserStore } = useCurrentUser()
-    const { clearGamesStore } = useGame()
     const { withdrawal } = useAuth()
     const userId = 1
 
     await withdrawal(userId)
-    expect(setLoading).toHaveBeenCalledOnce()
-    expect(deleteUser).toHaveBeenCalledOnce()
-    expect(firebaseWithdrawal).toHaveBeenCalledOnce()
-    expect(clearCurrentUserStore).toHaveBeenCalledOnce()
-    expect(clearGamesStore).toHaveBeenCalledOnce()
-    expect(clearLoading).toHaveBeenCalledOnce()
+    expect(mocks.setLoading).toHaveBeenCalledOnce()
+    expect(mocks.deleteUser).toHaveBeenCalledWith(userId)
+    expect(mocks.firebaseWithdrawal).toHaveBeenCalledOnce()
+    expect(mocks.clearCurrentUserStore).toHaveBeenCalledOnce()
+    expect(mocks.clearGamesStore).toHaveBeenCalledOnce()
+    expect(mocks.navigateTo).toHaveBeenCalledWith('/withdrawal')
+    expect(mocks.clearLoading).toHaveBeenCalledOnce()
   })
 
   it('if Firebase Withdrawal fails, user should not be able to withdrawal', async () => {
-    mocks.useFirebaseAuth.mockReturnValue({
-      user: vi.fn(),
-      isLoggedIn: vi.fn(),
-      firebaseLogin: vi.fn(),
-      firebaseLogout: vi.fn(),
-      checkAuthState: vi.fn(),
-      firebaseWithdrawal: vi.fn().mockRejectedValue(new Error())
-    })
-
-    const { setLoading, clearLoading } = useLoading()
-    const { firebaseWithdrawal } = useFirebaseAuth()
-    const { clearCurrentUserStore } = useCurrentUser()
-    const { clearGamesStore } = useGame()
+    mocks.firebaseWithdrawal.mockRejectedValueOnce(new Error())
+    mocks.value.mockReturnValueOnce(true)
     const { withdrawal } = useAuth()
     const userId = 1
 
     await withdrawal(userId)
-    expect(setLoading).toHaveBeenCalledOnce()
-    expect(deleteUser).toHaveBeenCalledOnce()
-    expect(firebaseWithdrawal).toHaveBeenCalledOnce()
-    expect(clearCurrentUserStore).not.toHaveBeenCalledOnce()
-    expect(clearGamesStore).not.toHaveBeenCalledOnce()
-    expect(clearLoading).toHaveBeenCalledOnce()
+    expect(mocks.setLoading).toHaveBeenCalledOnce()
+    expect(mocks.deleteUser).toHaveBeenCalledWith(userId)
+    expect(mocks.firebaseWithdrawal).toHaveBeenCalledOnce()
+    expect(mocks.clearCurrentUserStore).not.toHaveBeenCalledOnce()
+    expect(mocks.clearGamesStore).not.toHaveBeenCalledOnce()
+    expect(mocks.clearLoading).toHaveBeenCalledOnce()
+    expect(mocks.navigateTo).not.toHaveBeenCalledWith('/withdrawal')
+    expect(mocks.postUser).toHaveBeenCalledOnce()
+    expect(mocks.notifyOnSpot).toHaveBeenCalledWith('退会に失敗しました', 'error')
   })
 
   it('if Delete User fails, user should not be able to withdrawal', async () => {
-    mocks.useFirebaseAuth.mockReturnValue({
-      user: vi.fn(),
-      isLoggedIn: vi.fn(),
-      firebaseLogin: vi.fn(),
-      firebaseLogout: vi.fn(),
-      checkAuthState: vi.fn(),
-      firebaseWithdrawal: vi.fn().mockRejectedValue(new Error())
-    })
-
     mocks.deleteUser.mockRejectedValue(new Error())
-
-    const { setLoading, clearLoading } = useLoading()
-    const { firebaseWithdrawal } = useFirebaseAuth()
-    const { clearCurrentUserStore } = useCurrentUser()
-    const { clearGamesStore } = useGame()
     const { withdrawal } = useAuth()
     const userId = 1
 
     await withdrawal(userId)
-    expect(setLoading).toHaveBeenCalledOnce()
-    expect(deleteUser).toHaveBeenCalledOnce()
-    expect(firebaseWithdrawal).not.toHaveBeenCalledOnce()
-    expect(clearCurrentUserStore).not.toHaveBeenCalledOnce()
-    expect(clearGamesStore).not.toHaveBeenCalledOnce()
-    expect(clearLoading).toHaveBeenCalledOnce()
+    expect(mocks.setLoading).toHaveBeenCalledOnce()
+    expect(mocks.deleteUser).toHaveBeenCalledWith(userId)
+    expect(mocks.firebaseWithdrawal).not.toHaveBeenCalledOnce()
+    expect(mocks.clearCurrentUserStore).not.toHaveBeenCalledOnce()
+    expect(mocks.clearGamesStore).not.toHaveBeenCalledOnce()
+    expect(mocks.clearLoading).toHaveBeenCalledOnce()
+    expect(mocks.notifyOnSpot).toHaveBeenCalledWith('退会に失敗しました', 'error')
   })
 })
