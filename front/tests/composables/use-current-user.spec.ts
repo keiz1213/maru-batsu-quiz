684817c72d7e35ef546456c5f755a16f7685e91a
keiz1213 <fukuoka00tsc@gmail.com>
2023-09-03 11:48:18 +0900

    use-current-userのテストをリファクタ

--- front/tests/composables/use-current-user.spec.ts
+++ front/tests/composables/use-current-user.spec.ts
@@ -11,12 +11,7 @@ const mocks = vi.hoisted(() => {
   return {
     useFirebaseAuth: vi.fn(() => {
       return {
-        user: vi.fn(),
-        isLoggedIn: {},
-        firebaseLogin: vi.fn(),
-        firebaseLogout: vi.fn(),
-        checkAuthState: vi.fn(),
-        firebaseWithdrawal: vi.fn()
+        isLoggedIn: {}
       }
     }),
     postUser: vi.fn()
@@ -39,117 +34,94 @@ vi.mock('~/utils/api/services/user', async () => {
   }
 })
 
+const defaultCurrentUser = {
+  id: 0,
+  uid: '',
+  name: '',
+  avatar_url: '',
+  games: []
+}
+
+const authenticatedUser = {
+  id: 1,
+  uid: 'test',
+  name: 'test user',
+  avatar_url: 'https://avatars.githubusercontent.com/u/72614612?v=4',
+  games: []
+}
+
+const game = {
+  user_id: 1,
+  id: 1,
+  channel_name: 'test name',
+  title: 'Test Game',
+  description: 'This is a test game',
+  quizzes: [
+    {
+      question: '1 + 1 = 2 ?',
+      correct_answer: '◯',
+      explanation: '普通に2です'
+    },
+    {
+      question: '2 + 2 = 5 ?',
+      correct_answer: '✕',
+      explanation: '普通に4です'
+    },
+    {
+      question: '3 + 3 = 4 ?',
+      correct_answer: '✕',
+      explanation: '普通に6です'
+    }
+  ],
+  number_of_winner: 3,
+  created_at: '2023-08-04T12:34:56Z',
+  updated_at: '2023-08-04T14:22:33Z'
+}
+
+const owner = {
+  id: 1,
+  uid: 'test',
+  name: 'test user',
+  avatar_url: 'https://avatars.githubusercontent.com/u/72614612?v=4',
+  games: [game]
+}
+
 it('default currentUser', () => {
-  const defaultCurrentUser = {
-    id: 0,
-    uid: '',
-    name: '',
-    avatar_url: '',
-    games: []
-  }
   const { currentUser } = useCurrentUser()
   expect(currentUser.value).toEqual(defaultCurrentUser)
 })
 
-it('can set currentUser', () => {
-  const authenticatedUser = {
-    id: 1,
-    uid: 'test',
-    name: 'test user',
-    avatar_url: 'https://avatars.githubusercontent.com/u/72614612?v=4',
-    games: []
-  }
-
-  const { currentUser, setCurrentUserStore } = useCurrentUser()
-  setCurrentUserStore(authenticatedUser)
-  expect(currentUser.value).toEqual(authenticatedUser)
+describe('setCurrentUserStore', () => {
+  it('can set currentUser', () => {
+    const { currentUser, setCurrentUserStore } = useCurrentUser()
+    setCurrentUserStore(authenticatedUser)
+    expect(currentUser.value).toEqual(authenticatedUser)
+  })
 })
 
-it('can clear currentUser', () => {
-  const defaultCurrentUser = {
-    id: 0,
-    uid: '',
-    name: '',
-    avatar_url: '',
-    games: []
-  }
-
-  const authenticatedUser = {
-    id: 1,
-    uid: 'test',
-    name: 'test user',
-    avatar_url: 'https://avatars.githubusercontent.com/u/72614612?v=4',
-    games: []
-  }
-
-  const { currentUser, setCurrentUserStore, clearCurrentUserStore } =
-    useCurrentUser()
-  setCurrentUserStore(authenticatedUser)
-  expect(currentUser.value).toEqual(authenticatedUser)
-  clearCurrentUserStore()
-  expect(currentUser.value).toEqual(defaultCurrentUser)
+describe('clearCurrentUserStore', () => {
+  it('can clear currentUser', () => {
+    const { currentUser, setCurrentUserStore, clearCurrentUserStore } =
+      useCurrentUser()
+    setCurrentUserStore(authenticatedUser)
+    expect(currentUser.value).toEqual(authenticatedUser)
+    clearCurrentUserStore()
+    expect(currentUser.value).toEqual(defaultCurrentUser)
+  })
 })
-
-it('can be determined whether the user is the owner', () => {
-  const game = {
-    user_id: 1,
-    id: 1,
-    channel_name: 'test name',
-    title: 'Test Game',
-    description: 'This is a test game',
-    quizzes: [
-      {
-        question: '1 + 1 = 2 ?',
-        correct_answer: '◯',
-        explanation: '普通に2です'
-      },
-      {
-        question: '2 + 2 = 5 ?',
-        correct_answer: '✕',
-        explanation: '普通に4です'
-      },
-      {
-        question: '3 + 3 = 4 ?',
-        correct_answer: '✕',
-        explanation: '普通に6です'
-      }
-    ],
-    number_of_winner: 3,
-    created_at: '2023-08-04T12:34:56Z',
-    updated_at: '2023-08-04T14:22:33Z'
-  }
-
-  const owner = {
-    id: 1,
-    uid: 'test',
-    name: 'test user',
-    avatar_url: 'https://avatars.githubusercontent.com/u/72614612?v=4',
-    games: [game]
-  }
-
-  const { setCurrentUserStore, isOwner } = useCurrentUser()
-  setCurrentUserStore(owner)
-  expect(isOwner(game)).toBeTruthy()
+describe('isOwner', () => {
+  it('can be determined whether the user is the owner', () => {
+    const { setCurrentUserStore, isOwner } = useCurrentUser()
+    setCurrentUserStore(owner)
+    expect(isOwner(game)).toBeTruthy()
+  })
 })
 
 describe('checkCurrentUserStore', () => {
   describe('when loggedIn', () => {
     it('if the current user is the default current user, it can be reset.', () => {
-      const defaultCurrentUser = {
-        id: 0,
-        uid: '',
-        name: '',
-        avatar_url: '',
-        games: []
-      }
-
       mocks.useFirebaseAuth.mockReturnValueOnce({
-        user: vi.fn(),
-        isLoggedIn: { value: true },
-        firebaseLogin: vi.fn(),
-        firebaseLogout: vi.fn(),
-        checkAuthState: vi.fn(),
-        firebaseWithdrawal: vi.fn()
+        isLoggedIn: { value: true }
       })
 
       const { checkCurrentUserStore, currentUser } = useCurrentUser()
@@ -158,15 +130,11 @@ describe('checkCurrentUserStore', () => {
       expect(mocks.postUser).toHaveBeenCalledOnce()
     })
   })
+
   describe('when not loggedIn', () => {
     it('can not reset current user', () => {
       mocks.useFirebaseAuth.mockReturnValueOnce({
-        user: vi.fn(),
-        isLoggedIn: { value: false },
-        firebaseLogin: vi.fn(),
-        firebaseLogout: vi.fn(),
-        checkAuthState: vi.fn(),
-        firebaseWithdrawal: vi.fn()
+        isLoggedIn: { value: false }
       })
       const { checkCurrentUserStore } = useCurrentUser()
       checkCurrentUserStore()
