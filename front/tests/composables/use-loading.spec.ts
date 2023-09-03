c96b1871ddb14303b50cc64adb06b78d9fbc4a9d
keiz1213 <fukuoka00tsc@gmail.com>
2023-09-03 12:03:08 +0900

    use-loadingのテストをリファクタ

--- front/tests/composables/use-loading.spec.ts
+++ front/tests/composables/use-loading.spec.ts
@@ -11,17 +11,24 @@ it('default loading is false', () => {
   expect(loading.value).toBeFalsy()
 })
 
-it('switching from false to true for loading.', () => {
-  const { loading, setLoading } = useLoading()
-  expect(loading.value).toBeFalsy()
-  setLoading()
-  expect(loading.value).toBeTruthy()
+describe('setLoading', () => {
+  it('switching from false to true for loading.', () => {
+    const { loading, setLoading } = useLoading()
+    expect(loading.value).toBeFalsy()
+    setLoading()
+    expect(loading.value).toBeTruthy()
+  })
 })
 
-it('switching from true to false for loading.', () => {
-  const { loading, setLoading, clearLoading } = useLoading()
-  setLoading()
-  expect(loading.value).toBeTruthy()
-  clearLoading()
-  expect(loading.value).toBeFalsy()
+describe('clearLoading', () => {
+  it('switching from true to false for loading.', () => {
+    const { loading, setLoading, clearLoading } = useLoading()
+    setLoading()
+    expect(loading.value).toBeTruthy()
+    clearLoading()
+    expect(loading.value).toBeFalsy()
+  })
 })
+
+
+
